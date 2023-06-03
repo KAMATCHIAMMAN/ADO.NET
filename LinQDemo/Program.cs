3fd303f1ac548377bbed0da423be6056f8467cad
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-06-03 11:13:40 +0530

    Add files via upload

---
+++ LinQDemo/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LinQDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //*****LINQ INTRO********
+
+            //string[] employeedetails = { "kamatchi", "srimathi", "kavi" };
+            //IEnumerable<string> Evenlength = from name in employeedetails
+            //                                 where name.Length % 2 == 0
+            //                                 select name;
+            //foreach(string name in Evenlength)
+            //{
+            //    Console.WriteLine(name);
+            //
+            //
+            //}
+
+            ///****AGGREGATE FUNCTION**************
+
+            //int[] num = { 2, 3, 4, 5, };
+            //int total = num.Aggregate((a, b) => a + b);
+            //Console.WriteLine(total);
+
+
+
+            //Console.WriteLine(num.Count());
+            //Console.WriteLine(num.Min());
+            //Console.WriteLine(num.Max());
+            //Console.WriteLine(num.Sum());
+
+
+            var studentlist = from student in Students.StudentDetails()
+                              orderby student.rollNumber descending
+                              select student;
+
+            //foreach(var student in studentlist)
+            //{
+            //    Console.WriteLine(student.rollNumber+" "+student.studentName);
+            //}
+
+
+
+            //var studenrlisttwo = Students.StudentDetails().OrderBy(s => s.gender).ThenBy(n => n.studentName);
+
+
+
+            //*****Elementry level functions************
+
+
+            var studentone = Students.StudentDetails().ElementAt(2);
+
+            Console.WriteLine("student at second place " +studentone.studentName);
+
+            var studenttwo = Students.StudentDetails().ElementAtOrDefault(4);
+            Console.WriteLine("student at defult place" + studenttwo.studentName);
+
+
+            var studentthree = Students.StudentDetails().First();
+            Console.WriteLine("First student name" + studentthree.studentName);
+
+
+            var studentfour = Students.StudentDetails().Last();
+            Console.WriteLine("First student name" + studentfour.studentName);
+
+            var studentfive= Students.StudentDetails().LastOrDefault();
+            Console.WriteLine(studentfive.studentName);
+
+            var studentsix = Students.StudentDetails().Single();
+            Console.WriteLine(studentsix.studentName);
+
+            var studentseven=Students.StudentDetails().SingleOrDefault();
+
+
+            int[] numbrs = { 1, 3 };
+            var elements = numbrs.DefaultIfEmpty();
+            foreach(var item in elements)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ LinQDemo/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQDemo
+{
+    public class Students
+    {
+        public string rollNumber { get; set; }
+        public string studentName { get; set; }
+        public string department { get; set; }
+        public string gender { get; set; }
+        public static List<Students> StudentDetails()
+        {
+            List<Students> students = new List<Students> 
+            { 
+            new Students{rollNumber="19cse001",studentName="anuj",department="cse",gender="male"},
+            new Students{rollNumber="19cse003",studentName="balamurugan",department="cse",gender="male"},
+            new Students{rollNumber="19cse002",studentName="arun",department="ece",gender="male"},
+            new Students{rollNumber="19cse004",studentName="deevija",department="ece",gender="female"},
+            new Students{rollNumber="19cse005",studentName="kamatchi",department="civil",gender="female"},
+            new Students{rollNumber="19cse006",studentName="dharani",department="cse",gender="female"},
+            };
+            return students;
+        }
+    }
+}
